8ef0d996033ec805bc18b5383070198812fb6fee
jwizzle <info@hossel.net>
2025-03-16 23:37:48 +0100

    Prettify the discord dice template

--- src/dungeondice/lib/discord_templates.py
+++ src/dungeondice/lib/discord_templates.py
@@ -3,16 +3,19 @@ from dungeondice.lib import dice
 
 
 def dicerolls(author: str, rollgroups: list[dice.Rollgroup], comment: str):
+    comment = "__{}__".format(comment) if comment else comment
     out = ''
+
     for rg in rollgroups:
         out += '''\
+---------------------------------
 > {}
 **Total: {}**
 _Details: {}_
-
+---------------------------------
 '''.format(rg.rollstring, rg.total, rg.rollsets)
 
     return '''\
-{} rolled {}
+**{}** rolled {}
 {}
 '''.format(author, comment, out)
