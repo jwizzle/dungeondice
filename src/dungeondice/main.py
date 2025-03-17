78932f120c61444e44f102002c6fe8442edabb55
jwizzle <info@hossel.net>
2025-03-17 22:50:38 +0100

    Split out rollcomands to a commandgroup
    So we can later add the private rolling, while they use the same error
    handler. Also helps cleaning up the main.py.

--- src/dungeondice/main.py
+++ src/dungeondice/main.py
@@ -7,8 +7,7 @@ import os
 import sys
 from dotenv import load_dotenv
 
-from dungeondice.lib import dice
-from dungeondice.lib import discord_templates
+from dungeondice.lib.discord.rollcommands import Rollcommands
 
 
 load_dotenv()
@@ -20,8 +19,6 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-diceparser = dice.Parser()
-
 
 @bot.event
 async def on_ready():
@@ -31,51 +28,7 @@ async def on_ready():
     else:
         print('Something is seriously wrong with this bot.')
 
-
-@bot.command(
-    name='roll',
-    aliases=['r'],
-    description='roll',
-    brief='example: "2x2d20(poison)+d8(piercing)-4"',
-    help='''Roll dice in a format like "2x2d20(poison)+d8(piercing)-4"
-
-Rolls consist of multiple layers. The parser first cuts up rollstrings
-into multiple 'groups' of 'sets' by parsing all the x and , modifiers.
-The 'x' being a multiplier that creates multiple of the same rollgroups.
-The ',' being a separator that allows you to create multiple different
-groups in one go.
-Everything behind the 'x' modifier is treated as part of the multiplier
-until terminated by a ','.
-
-Examples:
-2xd20+d10:     Roll d20+d10 twice. Returning two different groups with
-                their own totals.
-d20,d20:       Roll a d20 twice. Returning two different groups with their
-                own totals. In this case it being the total of 1 dice.
-2xd20+d10,d10: Roll d20+d10 twice, roll d10 once. Returning three different
-                groups with their own totals.
-''',
-)
-async def roll(
-    ctx,
-    dicestring: dice.rollstring,
-    *, message: str = ''
-):
-    """Uses the diceparser to roll dice."""
-    await ctx.send(
-        discord_templates.dicerolls(
-            ctx.author.display_name, diceparser.parse(dicestring), message
-        )
-    )
-
-
-@roll.error
-async def roll_error(ctx, error):
-    if isinstance(error, commands.BadArgument):
-        await ctx.send('Invalid dicestring.')
-    else:
-        print(error)
-        await ctx.send('Uncaught error while rolling.')
+    await bot.add_cog(Rollcommands())
 
 
 def start_bot():
